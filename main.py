c4bffff35bd1153b4c80838199ee9cf9b07c8e90
Brant Faircloth <brant.faircloth@gmail.com>
2009-08-27 22:24:34 -0700

    Cleaning up.
    Not much to see here other than cleanup.  Added several TODOs.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-parser.py
+main.py
 
 Created by Brant Faircloth on 2009-08-14.
 Copyright (c) 2009 Brant Faircloth. All rights reserved.
@@ -67,32 +67,38 @@ def trim(record, left=None, right=None):
 def matches(tag, seq_match_span, tag_match_span, allowed_errors):
     # deal with case where tag match might be perfect, but extremely gappy, 
     #e.g. ACGTCGTGCGGA-------------------------ATC
-    if tag_match_span.count('-') > allowed_errors or seq_match_span.count('-') > allowed_errors:
+    if tag_match_span.count('-') > allowed_errors or seq_match_span.count('-')\
+     > allowed_errors:
         return 0, 0
     # we don't really want to penalize mismatches/gaps with -1, rather we
     # just don't want to count them as matches (=0).  The case above should
     # help with long extensions across gaps
     else:
         #pdb.set_trace()
-        seq_array, tag_array = numpy.array(list(seq_match_span)), numpy.array(list(tag_match_span))
+        seq_array, tag_array = numpy.array(list(seq_match_span)), \
+        numpy.array(list(tag_match_span))
         matches = sum(seq_array == tag_array)
-        error = sum(seq_array != tag_array) + (len(tag) - len(tag_match_span.replace('-','')))
-        # Original scoring method from http://github.com/chapmanb/bcbb/tree/master treats gaps
-        # incorrectly:
-        #return sum((1 if s == tag_match_span[i] else 0) for i, s in enumerate(seq_match_span))
+        error = sum(seq_array != tag_array) + (len(tag) - \
+        len(tag_match_span.replace('-','')))
+        # Original scoring method from 
+        # http://github.com/chapmanb/bcbb/tree/master treats gaps incorrectly:
+        #return sum((1 if s == tag_match_span[i] else 0) for i, s in 
+        #enumerate(seq_match_span))
         return matches, error
 
 def smithWaterman(seq, tags, allowed_errors):
     '''Borrowed & heavily modified from http://github.com/chapmanb/bcbb/tree/master'''
     #if seq == 'CGAGAGATACAAAAGCAGCAGCGGAATCGATTCCGCTGCTGC':
     #    pdb.set_trace()
-    high_score = {'tag':None, 'seq_match':None, 'mid_match':None, 'score':None, 'start':None, 'end':None, 'matches':None, 'errors':allowed_errors}
+    high_score = {'tag':None, 'seq_match':None, 'mid_match':None, 'score':None, 
+        'start':None, 'end':None, 'matches':None, 'errors':allowed_errors}
     for tag in tags:
         seq_match, tag_match, score, start, end = pairwise2.align.localms(seq, 
-            tag, 5.0, -4.0, -9.0, -0.5, one_alignment_only=True)[0]
+        tag, 5.0, -4.0, -9.0, -0.5, one_alignment_only=True)[0]
         seq_match_span, tag_match_span = seq_match[start:end], tag_match[start:end]
         match, errors = matches(tag, seq_match_span, tag_match_span, allowed_errors)
-        if match >= len(tag)-allowed_errors and match > high_score['matches'] and errors <= high_score['errors']:
+        if match >= len(tag)-allowed_errors and match > high_score['matches'] \
+        and errors <= high_score['errors']:
             high_score['tag'] = tag
             high_score['seq_match'] = seq_match
             high_score['tag_match'] = tag_match
@@ -103,7 +109,9 @@ def smithWaterman(seq, tags, allowed_errors):
             high_score['seq_match_span'] = seq_match_span
             high_score['errors'] = errors
     if high_score['matches']:
-        return high_score['tag'], high_score['matches'], high_score['seq_match'], high_score['seq_match_span'], high_score['start'], high_score['end']
+        return high_score['tag'], high_score['matches'], \
+        high_score['seq_match'], high_score['seq_match_span'], \
+        high_score['start'], high_score['end']
     else:
         return None
 
@@ -205,16 +213,19 @@ def linkerTrim(record, tags, max_gap_char=5, **kwargs):
     right   = rightLinker(s, tags, max_gap_char=5, fuzzy=kwargs['fuzzy'])
     if left and right and left[0] == right[0]:
         # we can have lots of conditional matches here
-        if left[2] < max_gap_char and right[2] > (len(s) - (len(right[0]) + max_gap_char)):
+        if left[2] < max_gap_char and right[2] > (len(s) - (len(right[0]) + \
+        max_gap_char)):
             trimmed = trim(record, left[3], right[2])
             # left and right are identical so largely pass back the left
             # info... except for m_type which can be a combination
-            tag, m_type, seq_match = left[0], left[1]+'-'+right[1]+'-both', left[4]
+            tag, m_type, seq_match = left[0], left[1]+'-'+right[1]+'-both', \
+            left[4]
         else:
             pass
     elif left and right and left[0] != right[0]:
         # flag
-        if left[2] < max_gap_char and right[2] > (len(s) - (len(right[0]) + max_gap_char)):
+        if left[2] < max_gap_char and right[2] > (len(s) - (len(right[0]) + \
+        max_gap_char)):
             trimmed = None
             tag, m_type, seq_match = None, 'tag-mismatch', None
     elif left:
@@ -252,7 +263,14 @@ def createSeqTable(c):
         c.execute('''DROP TABLE sequence''')
     except:
         pass
-    c.execute('''CREATE TABLE sequence (id INT UNSIGNED NOT NULL AUTO_INCREMENT,name VARCHAR(100),mid VARCHAR(30),mid_seq VARCHAR(30),mid_match VARCHAR(30),mid_method VARCHAR(50),linker VARCHAR(30),linker_seq VARCHAR(30),linker_match VARCHAR(30),linker_method VARCHAR(50),cluster VARCHAR(50),concat_seq VARCHAR(30), concat_match varchar(30), concat_method VARCHAR(50),n_count SMALLINT UNSIGNED,untrimmed_len SMALLINT UNSIGNED,PRIMARY KEY (id))''')
+    c.execute('''CREATE TABLE sequence (id INT UNSIGNED NOT NULL 
+        AUTO_INCREMENT,name VARCHAR(100),mid VARCHAR(30),mid_seq VARCHAR(30),
+        mid_match VARCHAR(30),mid_method VARCHAR(50),linker VARCHAR(30),
+        linker_seq VARCHAR(30),linker_match VARCHAR(30),linker_method 
+        VARCHAR(50),cluster VARCHAR(50),concat_seq VARCHAR(30), 
+        concat_match varchar(30), concat_method VARCHAR(50),
+        n_count SMALLINT UNSIGNED,untrimmed_len SMALLINT UNSIGNED,
+        PRIMARY KEY (id))''')
 
 def concatCheck(record, all_tags, all_tags_regex, reverse_linkers, **kwargs):
     s = str(record.seq)
@@ -305,37 +323,40 @@ def worker(record, qual, tags, all_tags, all_tags_regex, reverse_mid, reverse_li
         if linker:
             l_tag, l_trimmed, l_seq_match, l_critter, l_m_type = linker
         else:
-            l_tag, l_trimmed, l_seq_match, l_critter, l_m_type, concat_type, concat_count = (None,) * 7
+            l_tag, l_trimmed, l_seq_match, l_critter, l_m_type, concat_type, \
+            concat_count = (None,) * 7
     else:
         mid, trimmed, seq_match, m_type = (None,) * 4
-        l_tag, l_trimmed, l_seq_match, l_critter, l_m_type, concat_type, concat_count = (None,) * 7
+        l_tag, l_trimmed, l_seq_match, l_critter, l_m_type, concat_type, \
+        concat_count = (None,) * 7
     # check for concatemers
-    #if record.id == 'MID15_NoError_SimpleX1_NoError_SimpleX1_CONCAT_bog_copper_1ee':
-    #    pdb.set_trace()
     concat_check = True
     if concat_check:
         if l_trimmed and len(l_trimmed.seq) > 0:
-            concat_tag, concat_type, concat_seq_match = concatCheck(l_trimmed, all_tags, all_tags_regex, reverse_linkers, fuzzy=True)
+            concat_tag, concat_type, concat_seq_match = concatCheck(l_trimmed, 
+                all_tags, all_tags_regex, reverse_linkers, fuzzy=True)
         else:
             concat_tag, concat_type, concat_seq_match = None, None, None
     else:
         concat_tag, concat_type, concat_seq_match = None, None, None
-    #concat_type = None
-    #pdb.set_trace()
-    cur.execute("INSERT INTO sequence (name, mid, mid_seq, mid_match, mid_method, linker, linker_seq, linker_match, linker_method, cluster, concat_seq, concat_match, concat_method, n_count, untrimmed_len) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s)", (record.id, reverse_mid[mid], mid, seq_match, m_type, reverse_linkers[l_tag], l_tag, l_seq_match, l_m_type, l_critter, concat_tag, concat_seq_match, concat_type, N_count, untrimmed_len,))
+    cur.execute('''INSERT INTO sequence (name, mid, mid_seq, mid_match, 
+        mid_method, linker, linker_seq, linker_match, linker_method, cluster, 
+        concat_seq, concat_match, concat_method, n_count, untrimmed_len) 
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s)''', (record.id,
+        reverse_mid[mid], mid, seq_match, m_type, reverse_linkers[l_tag], 
+        l_tag, l_seq_match, l_m_type, l_critter, concat_tag, concat_seq_match, 
+        concat_type, N_count, untrimmed_len,))
     cur.close()
     conn.commit()
     conn.close()
     return
 
 def main():
-    #pdb.set_trace()
     start_time = time.time()
     conf = ConfigParser.ConfigParser()
     conf.read('mc454.conf')
     mid, reverse_mid = dict(conf.items('MID')), reverse(conf.items('MID'))
     linkers, reverse_linkers = dict(conf.items('Linker')), reverse(conf.items('Linker'))
-    pdb.set_trace()
     reverse_mid[None] = None
     reverse_linkers[None] = None
     clust = conf.items('Clusters')
@@ -350,29 +371,38 @@ def main():
     createSeqTable(cur)
     cur.close()
     conn.close()
-    # for each sequence
-    record = QualityIO.PairedFastaQualIterator(open(conf.get('Input','sequence'), "rU"), open(conf.get('Input','qual'), "rU"))
+    record = QualityIO.PairedFastaQualIterator(
+    open(conf.get('Input','sequence'), "rU"), 
+    open(conf.get('Input','qual'), "rU"))
     #pdb.set_trace()
     if conf.getboolean('Multiprocessing', 'MULTIPROCESSING'):
         # get num processors
         n_procs = conf.get('Multiprocessing','processors')
         if n_procs == 'Auto':
+            # TODO:  change this?
+            # we'll start 2X-1 threads (X = processors).
             n_procs = (multiprocessing.cpu_count() - 1)*2
         else:
             n_procs = int(n_procs)*2
         print 'Multiprocessing.  Number of processors = ', n_procs
+        # to test with fewer sequences
         #count = 0
         try:
+            # TODO:  move this to more pool-based list of processes 
+            # (see example on intertubes)
             jobs = [None] * n_procs
+            # to test with fewer sequences
             #while count < 5000:
-                #pdb.set_trace()
+            # to test with all sequences
             while record:
                 for i in range(n_procs):
+                    # to test with fewer sequences
                     #count +=1
-                    p = multiprocessing.Process(target=worker, args=(record.next(), qual, tags, all_tags, all_tags_regex, reverse_mid, reverse_linkers))
+                    p = multiprocessing.Process(target=worker, args=(
+                    record.next(), qual, tags, all_tags, all_tags_regex, 
+                    reverse_mid, reverse_linkers))
                     jobs[i]=p
                     p.start()
-                    #p.join()
                 for j in jobs:
                     try:
                         j.join()
@@ -382,11 +412,13 @@ def main():
             pass
     else:
         print 'Not using multiprocessing'
-        count = 0
+        #count = 0
         try:
-            while count < 1000:
-                count +=1
-                worker(record.next(), qual, tags, all_tags, all_tags_regex, reverse_mid, reverse_linkers)
+            #while count < 1000:
+            while record:
+                #count +=1
+                worker(record.next(), qual, tags, all_tags, all_tags_regex, 
+                reverse_mid, reverse_linkers)
         except StopIteration:
             pass
     end_time = time.time()
